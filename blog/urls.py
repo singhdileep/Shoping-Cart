d6765131d3f9d5dcda307e7e835b14af2564124c
Dileep singh <rajsingh@Dileeps-MacBook-Pro.local>
2020-05-06 23:47:57 +0530

    Commit till the search pages

--- blog/urls.py
+++ blog/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path("",views.index,name = "Blog Home")
+    path("", views.index, name="blogHome"),
+    path("blogpost/<int:id>", views.blogpost, name="blogHome")
 ]
+ 
