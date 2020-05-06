780292850493074478b58fd81c1f82fe4c9ffea2
Dileep singh <rajsingh@Dileeps-MacBook-Pro.local>
2020-05-07 01:38:51 +0530

    complete till search and track section

--- shop/views.py
+++ shop/views.py
@@ -75,16 +75,14 @@ def tracker(request):
                 updates = []
                 for item in update:
                     updates.append({'text': item.update_desc, 'time': item.timestamp})
-                    response = json.dumps([updates, order[0].items_json], default=str)
+                    response = json.dumps({"status":"success", "updates": updates, "itemsJson": order[0].items_json}, default=str)
                 return HttpResponse(response)
             else:
-                return HttpResponse('{}')
+                return HttpResponse('{"status":"noitem"}')
         except Exception as e:
-            return HttpResponse('{}')
+            return HttpResponse('{"status":"error"}')
 
     return render(request, 'shop/tracker.html')
-
-
 def products(request, myid):
 
     # Fetch the product using the id
